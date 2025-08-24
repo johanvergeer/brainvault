76ce9b6d67f0b30d7b4a01c07fbe891a99a40347
Johan Vergeer <johan.vergeer@alliander.com>
2025-08-24 10:48:04 +0200

    Add constant for Earth Gravity

---
+++ utils/src/main/kotlin/constants/constants.kt
@@ -0,0 +1,17 @@
+package com.brainvault.utils.constants
+
+import com.brainvault.utils.units.mps2
+
+/**
+ * Standard gravitational acceleration on Earth.
+ *
+ * Defined as:
+ * \[
+ * g \approx 9.81 \,\text{m/s}^2
+ * \]
+ *
+ * This constant is commonly used in mechanics and physics calculations.
+ *
+ * @usesMathJax
+ */
+val EARTH_GRAVITY = 9.81.mps2
